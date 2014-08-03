f967938c898ca3e0fcd34cf56dd8cc766149fcd8
Giovanni Ferron <gvnnrulez@gmail.com>
2014-08-03 23:18:17 +1000

    Fix calculation and sys args bugs
    Fixed calculation of a polygon perimeter.
    
    - main function renamed to calculate length for unite testing.
    - if there is only one point the distance array is empty, if 2 point
    there is only one line, for more than 2 there are N lines for M points
    - added feature in sys args to load all the csv files

--- perimeter.py
+++ perimeter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import csv
+import glob
+import os
 import sys
 
 
-def main(file_name):
+def calculate_length(file_name):
     """
     read a list of points from a CSV file and print out the length of the perimeter of the shape that is formed by
     joining the points in their listed order
@@ -13,13 +15,19 @@ def main(file_name):
 
     points = []
     for row in reader:
-        x = row[0]
-        y = row[1]
-        points.append((x, y))
+        x = try_parse(row[0])
+        y = try_parse(row[1])
+        if x is not None and y is not None:
+            points.append((x, y))
 
-    length = perimeter(points)
+    return perimeter(points)
 
-    print length
+
+def try_parse(string, fail=None):
+    try:
+        return float(string)
+    except Exception:
+        return fail
 
 
 def perimeter(points):
@@ -37,16 +45,29 @@ def get_distances(points):
     """ convert a list of points into a list of distances """
     i = 0
     distances = []
+
+    if len(points) == 1:  # single point, no distance
+        return distances
+
     for i in range(len(points)):
         point = points[i]
-        next_point = points[i + 1]
-        x0 = point[0]
-        y0 = point[1]
-        x1 = next_point[1]
-        y1 = next_point[1]
 
-        point_distance = get_distance(x0, y0, x1, y1)
-        distances.append(point_distance)
+        if len(points) > i + 1:
+            next_point = points[i + 1]
+        else:
+            if len(points) > 2:
+                next_point = points[0]
+            else:
+                next_point = None  # 2 point it's a line, only one distance
+
+        if next_point is not None:
+            x0 = point[0]
+            y0 = point[1]
+            x1 = next_point[0]
+            y1 = next_point[1]
+
+            point_distance = get_distance(x0, y0, x1, y1)
+            distances.append(point_distance)
 
     return distances
 
@@ -61,5 +82,16 @@ def get_distance(x1, y1, x2, y2):
 
 
 if __name__ == "__main__":
-    path = sys.argv[0]
-    main(path)
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        if os.path.isdir(path):
+            csvfiles = glob.glob("{}/*.csv".format(sys.argv[1]))
+            for csvfile in csvfiles:
+                print "{} \t {}".format(csvfile, calculate_length(csvfile))
+        else:
+            if os.path.isfile(path):
+                print calculate_length(path)
+            else:
+                print "File not found"
+    else:
+        print "Please enter file or folder path"
